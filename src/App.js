bd62435d8b8159006cdf8c579d82d9d34573e485
ahmedmumdoh <amumdoh@gmail.com>
2022-06-26 04:33:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -6,86 +6,89 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Search from "./components/Search";
 
-class BooksApp extends React.Component {
-  state = {
+function BooksApp() {
+  const [state, setState] = useState({
     showSearchPage: false,
     books: [],
     search: "",
     booksFromSearch: [],
-    loadSearch: false,
-  };
+    loadSearch: false
+  });
 
-  componentDidMount() {
+  useEffect(() => {
     BooksAPI.getAll().then((res) => {
-      this.setState({
-        books: res,
-      });
+      setState((prevState) => ({
+        ...prevState,
+        books: res
+      }));
     });
-  }
+  }, []);
 
-  changeShelf = async (book, shelf) => {
-    await BooksAPI.update(book, shelf);
-    await BooksAPI.getAll().then((res) => {
-      this.setState({
-        books: res,
-      });
-    });
-    this.handleBooksSearch(this.state.search)
-  };
-
-  handleSearch = async (event) => {
-    await this.setState({
-      search: event.target.value,
-    });
-    console.log(this.state.search);
-    this.handleBooksSearch(this.state.search);
-  };
-
-  handleBooksSearch = async (search) => {
+  const handleBooksSearch = async (search) => {
     await BooksAPI.search(search).then((res) => {
       if (res && !res.error) {
-        this.setState({
+        setState((prevState) => ({
+          ...prevState,
           booksFromSearch: res.map((booksSearch) => {
-            this.state.books.forEach((book) => {
-              if (booksSearch.id === book.id) booksSearch.shelf = book.shelf
-            })
+            prevState.books.forEach((book) => {
+              if (booksSearch.id === book.id) booksSearch.shelf = book.shelf;
+            });
             return booksSearch;
           }),
           loadSearch: true
-        });
+        }));
       } else {
-        this.setState({
-          booksFromSearch: `No books like: " ${this.state.search} "`,
+        setState((prevState) => ({
+          ...prevState,
+          booksFromSearch: `No books like: " ${state.search} "`,
           loadSearch: false
-        })
+        }));
       }
     }); // then
     console.log("Search");
-    console.log(this.state.booksFromSearch);
+    console.log(state.booksFromSearch);
   };
 
-  render() {
-    return (
-      <Router>
-        <div className="app">
-          <Switch>
-            <Route path="/search">
-              <Search
-                handleSearch={this.handleSearch}
-                search={this.state.search}
-                booksFromSearch={this.state.booksFromSearch}
-                changeShelf={this.changeShelf}
-                loadSearch={this.state.loadSearch}
-              />
-            </Route>
-            <Route path="/">
-              <Home books={this.state.books} changeShelf={this.changeShelf} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+  const changeShelf = async (book, shelf) => {
+    await BooksAPI.update(book, shelf);
+    await BooksAPI.getAll().then((res) => {
+      setState((prevState) => ({
+        ...prevState,
+        books: res
+      }));
+    });
+    handleBooksSearch(state.search);
+  };
+
+  const handleSearch = async (event) => {
+    await setState((prevState) => ({
+      ...prevState,
+      search: event.target.value
+    }));
+    console.log(state.search);
+    handleBooksSearch(state.search);
+  };
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/search">
+            <Search
+              handleSearch={handleSearch}
+              search={state.search}
+              booksFromSearch={state.booksFromSearch}
+              changeShelf={changeShelf}
+              loadSearch={state.loadSearch}
+            />
+          </Route>
+          <Route path="/">
+            <Home books={state.books} changeShelf={changeShelf} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default BooksApp;
